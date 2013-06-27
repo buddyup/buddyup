54503e7aecc12f0e958cffc736d79e77f597a38f
Aaron DeVore <aaron.devore@gmail.com>
2013-06-27 15:30:24 -0700

    Initialize g.user before request

--- buddyup/app.py
+++ buddyup/app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, g, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('BUDDYUP_SETTINGS', silent=True)
+
+import database
+
+@app.before_request
+def setup():
+    if 'uid' in request.session:
+        user = database.User.query.get(user_id)
+        # Invalid user id, kill the session with fire!
+        if user is None:
+            app.logger.warning("Session with uid %i is invalid, clearing session")
+            request.session.clear()
+    else:
+        g.user = None
+
+
+@app.teardown_request
+def teardown():
+    del g.user
 
 # Import after creating `app` to let pages.* have access to buddyup.app.app
 import pages
