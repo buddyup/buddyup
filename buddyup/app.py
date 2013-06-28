462d155e402ba570e60369c2faa86fa25ff5a3db
Aaron DeVore <aaron.devore@gmail.com>
2013-06-28 15:05:39 -0700

    Fixes on before_request/teardown of sessions

--- buddyup/app.py
+++ buddyup/app.py
@@ -1,26 +1,32 @@
-from flask import Flask, g, request
+from flask import Flask, g, session
+from flask.ext.runner import Runner
+
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('BUDDYUP_SETTINGS', silent=True)
 
 import database
+import templating
 
 @app.before_request
 def setup():
-    if 'uid' in request.session:
-        user = database.User.query.get(user_id)
+    if 'uid' in session:
+        user = database.User.query.get('user_id')
         # Invalid user id, kill the session with fire!
         if user is None:
             app.logger.warning("Session with uid %i is invalid, clearing session")
-            request.session.clear()
+            session.clear()
     else:
         g.user = None
 
 
 @app.teardown_request
-def teardown():
-    del g.user
+def teardown(*args):
+    if hasattr(g, 'user'):
+        del g.user
+
 
 # Import after creating `app` to let pages.* have access to buddyup.app.app
 import pages
