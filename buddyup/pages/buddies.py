7d3e7d89eb6b90728d7bbae8bf6b337554d08687
Aaron DeVore <aaron.devore@gmail.com>
2013-08-12 18:49:37 -0700

    Adjustments to buddies.py (still not done)

--- buddyup/pages/buddies.py
+++ buddyup/pages/buddies.py
@@ -3,13 +3,14 @@ from flask import g
 from buddyup.app import app
 from buddyup.templating import render_template
 from buddyup.util import login_required, args_get
+from buddyup.database import User
 
 @app.route("/buddy/view/<user_name>")
 @login_required
 def view_buddy(user_name):
-    buddy_record = User.query.filter_by(user_name=get(user_name))
+    buddy_record = User.query.filter_by(user_name=user_name).first()
     # TODO: Which template?
-    return render_template('buddy/view.html')
+    return render_template('buddy/view.html', buddy_record=buddy_record)
 
 
 @app.route("/buddy/search")
@@ -20,7 +21,7 @@ def search_buddies():
                            buddies=buddies)
 
 
-@app.route("/buddy/search_result/")
+@app.route("/buddy/search_result")
 @login_required
 def search_results_buddies():
     args_get('', default=u'')
