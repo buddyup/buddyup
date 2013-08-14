cc00aa7da1f85e36912a2279e471b4659522e9d3
Aaron DeVore <aaron.devore@gmail.com>
2013-08-13 18:22:51 -0700

    Add hour:minute to events_to_json

--- buddyup/pages/calendar.py
+++ buddyup/pages/calendar.py
@@ -10,11 +10,12 @@ def calendar(start, end):
 def events_to_json(events):
     json = []
     for event in events:
-        date = event.date.strftime("%Y-%m-%d")
+        date = event.date.strftime("%Y-%m-%d %H:%M")
         json.append({
             'id': event.id,
             'title': event.name,
             'start': date,
             'end': date,
+            'url': url_for('event_view', event_id=event.id),
         })
     return json
