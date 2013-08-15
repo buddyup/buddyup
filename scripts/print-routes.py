96727989ed3d220d27bdbed9b9e3d2e489822256
Aaron DeVore <aaron.devore@gmail.com>
2013-08-14 20:53:35 -0700

    route printing is now sorted

--- scripts/print-routes.py
+++ scripts/print-routes.py
@@ -4,5 +4,7 @@ import sys, os
 sys.path.insert(0, os.getcwd())
 
 from buddyup import app
-for rule in app.url_map.iter_rules():
+rules = sorted(app.url_map.iter_rules(), key=lambda rule: rule.endpoint)
+
+for rule in rules:
     print rule.endpoint, '->', rule
