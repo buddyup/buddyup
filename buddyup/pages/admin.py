4c12f812b87fb3446cf4d88c53ccd95b273a2792
Aaron DeVore <aaron.devore@gmail.com>
2013-08-14 21:03:32 -0700

    Add check for administrator where necessary

--- buddyup/pages/admin.py
+++ buddyup/pages/admin.py
@@ -1,19 +1,33 @@
+from flask import g
+
 from buddyup.app import app
 from buddyup.database import Course, Visit, db
 from buddyup.templating import render_template
-from functools import partial
+from functools import partial, wraps
+
+
+def admin_required(f):
+    @wraps(f)
+    def func(*args, **kwargs):
+        if g.user and g.user.user_name == app.config.get("ADMIN_USER", u""):
+            return f(*args, **kwargs)
+        else:
+            abort(403)
+    return func
 
 
 @app.route("/admin")
+@admin_required
 def admin_dashboard():
     return render_template('dashboard.html')
 
 
 @app.route("/admin/course/add", methods=['POST', 'GET'])
+@admin_required
 def admin_add_course():
-    get_int = partial(convert=int)
  
     if request.method == 'POST':
+        get_int = partial(form_get, convert=int)
         # TODO: Change to include
         crn = get_int('crn')
         name = form_get('name')
@@ -26,10 +40,12 @@ def admin_add_course():
                         number=number)
         db.session.add(course)
         db.session.commit()
+        flash("Added Course " + name)
     return render_template('add_course.html')
 
 
 @app.route("/admin/course/delete", methods=['POST', 'GET'])
+@admin_required
 def admin_delete_course():
     if request.method == 'POST':
         course_id = form_get('course_id', convert=int)
@@ -42,6 +58,7 @@ def admin_delete_course():
 
 
 @app.route("/admin/stats")
+@admin_required
 def admin_stats():
     variables = {}
     variables['group_count'] = Group.query.count()
