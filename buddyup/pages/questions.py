a3bccdba1fdb9572f143f7bc60cac246507ea4f3
WillNguyen <trung.nguyen.14@hotmail.com>
2013-08-18 19:31:49 -0700

    Implemented questions

--- buddyup/pages/questions.py
+++ buddyup/pages/questions.py
@@ -8,14 +8,14 @@ from buddyup.templating import render_template
 from buddyup.util import args_get, login_required, form_get, check_empty
 
 @app.route('/forum')
-def question_view_all():
+def view_all_question():
     # TODO: display about 10 questions per page in chronological order.
     # Use pagination?
     pass
 
 
 @app.route('/forum/<int:question_id>')
-def question_view(question_id):
+def view_question(question_id):
     question = Question.query.get_or_404(question_id)
     answers = Answer.query.filter_by(question_id=question.id).order_by(Answer.time)
     return render_template('view_question.html', question=question,
@@ -24,7 +24,7 @@ def question_view(question_id):
 
 @app.route('/forum/post_question', methods=['GET', 'POST'])
 @login_required
-def question_create():
+def create_question():
     if request.method == 'GET':
         return render_template('post_question.html', has_errors=False)
     else:
@@ -44,16 +44,41 @@ def question_create():
         db.session.commit()
         #TODO: refer to event_create
         #question_id = Question.query.filter_
-        return redirect(url_for('question_view', question_id=question_id))
+        return redirect(url_for('view_question', question_id=question_id))
+
+@app.route('/forum/edit/<int:question_id>', methods=['POST', 'GET'])
+@login_required
+def edit_quesion(question_id):
+    question = Question.query.filter_by(id==question_id, user_id==g.user.id).first_or_404()
+    
+    if request.method == 'GET':
+        return render_template('edit_question.html', question=question,
+                has_errors=False)
+    else:
+        title = form_get('title')
+        check_empty(title, "Title")
+        text = form_get('text')
+        check_empty(text, "Text")
+        time = datetime.now()
+        
+        if get_flashed_message():
+            return render_template('edit_quesion.html', question=question,
+                    has_errors=True)
+        
+        question.title = title
+        question.text = text
+        quesiton.time = time
+        db.session.update(question)
+        db.sesion.commit()
+
+        return redirect(url_for('view_question', question_id=question.id))
+
 
 @app.route('/forum/remove/<int:question_id>', methods=['POST'])
 @login_required
-def question_remove(question_id):
-    question = Question.query.filter_by(id=question_id, user_id=g.user.id)
-
-    if question is None:
-        abort(403)
-    else:
-        question.first().delete()
-        db.session.commit()
-        return redirect(url_for('question_view'))
+def remove_question(question_id):
+    question = Question.query.filter_by(id==question_id, user_id==g.user.id).first_or_404()
+    question.delete()
+        #TODO: delete its answers
+    db.session.commit()
+    return redirect(url_for('view_question'))
