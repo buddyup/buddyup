173743fb0c9e0578d8b0fac5e9ce65f3fbb0d433
Aaron DeVore <aaron.devore@gmail.com>
2013-09-12 18:24:21 -0700

    Register error handlers for all available error statuses

--- buddyup/pages/errorhandlers.py
+++ buddyup/pages/errorhandlers.py
@@ -2,7 +2,11 @@ from buddyup.app import app
 from buddyup.templating import render_template
 
 
+@app.errorhandler(400)
+@app.errorhandler(403)
 @app.errorhandler(404)
+@app.errorhandler(500)
+@app.errorhandler(503)
 def error_page(e):
     """
     Dispatch to {error code}.html
