f5d6e532910c88d3313309578ddc4115fecb9c66
Aaron DeVore <aaron.devore@gmail.com>
2013-09-24 04:36:26 -0700

    Add AWS_S3_BUCKET environmental variable

--- buddyup/app.py
+++ buddyup/app.py
@@ -14,12 +14,17 @@ app.config.from_object(config_object)
 # NO_CDN: Set the CDN environmental variable to use local files instead of
 # CDN files.
 app.config['USE_CDN'] = 'NO_CDN' not in os.environ
-if 'ADMIN_USER' in os.environ:
-    app.config['ADMIN_USER'] = os.environ['ADMIN_USER']
-if 'SECRET_KEY' in os.environ:
-    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-if 'HELP_URL' in os.environ:
-    app.config['HELP_URL'] = os.environ['HELP_URL']
+
+def from_env(*variables):
+    for variable in variables:
+        if variable in os.environ:
+            app.config[variable] = os.environ[variable]
+
+from_env('ADMIN_USER',
+         'SECRET_KEY',
+         'HELP_URL',
+         'AWS_S3_BUCKET',
+         )
 Heroku(app)
 
 runner = Runner(app)
