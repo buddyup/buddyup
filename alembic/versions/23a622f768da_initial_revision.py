ec1f0d5066ac53d0c1e6983e61165a3ab56e05f8
devskag <devskag@gmail.com>
2013-11-19 21:16:14 -0800

    alembic: Added an initial alembic revision. Removed Rob's test files.

---
+++ alembic/versions/23a622f768da_initial_revision.py
@@ -0,0 +1,22 @@
+"""Initial Revision
+
+Revision ID: 23a622f768da
+Revises: None
+Create Date: 2013-11-19 21:15:03.210863
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '23a622f768da'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
