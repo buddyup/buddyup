71b8d9134a62ea35981961a5f9fca28d380ece38
vunhatminh241191 <vnm241191@gmail.com>
2014-01-12 14:10:44 -0800

    Changing buddy_search function

--- buddyup/pages/buddies.py
+++ buddyup/pages/buddies.py
@@ -76,10 +76,18 @@ def buddy_search():
     majors = Major.query.all()
     languages = sorted_languages()
     locations = Location.query.order_by(Location.name).all()
+    buddies = g.user.buddies.all()
     for course in courses:
       users = User.query.join(Course.users).filter(Course.name == course.name).all()
       for user in users:
-        beta_classmates.append(user)
+        if user.id == g.user.id:
+          continue
+        elif:
+          for buddy in buddies:
+            if user.id == buddy.id:
+              continue
+        else:
+          beta_classmates.append(user)
     for user in beta_classmates:
       if i < 3 and k <= len(beta_classmates):
         alpha_classmates.append(user)
