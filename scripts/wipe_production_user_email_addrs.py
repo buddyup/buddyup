5c3a304f448c155363ed5524082e51cc3a356887
Aaron DeVore <aaron.devore@gmail.com>
2014-03-10 19:31:42 -0700

    Wipe emails just clears the email field now

--- scripts/wipe_production_user_email_addrs.py
+++ scripts/wipe_production_user_email_addrs.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 '''
 Modify all user email addresses to use DEFAULT_EMAIL_FORMAT instead.
-** WARNING ** : this is intended to be used in a development environment where you've imported production
-data.  This is destructive and will conver all user email addresses.
+
+** WARNING **
+
+This is intended to be used in a development environment where you've imported 
+production data.  This is destructive and will convert all user email 
+addresses.
 
 Example:
     john@smith.com ==> rbednark+john.smith.com@gmail.com
@@ -11,36 +15,20 @@ Example:
 import sys, os, argparse, io, logging, logging.handlers
 
 
-logger = logging.getLogger('wipse_email_addrs')
+logger = logging.getLogger('wipe_email_addrs')
 
 sys.path.insert(0, os.getcwd())
 
 from buddyup.app import app
 from buddyup.database import db, Location, Major, Language, Course, User
 
-EMAIL_FORMAT = app.config.get("DEFAULT_EMAIL_FORMAT", 'rbednark+{user}@gmail.com')
-
 parser = argparse.ArgumentParser()
 
-def change_email_addr(addr):
-    addr = addr.replace('@', '.')
-    new = EMAIL_FORMAT.format(user=addr)
-    return unicode(new)
 
 def change_user_email_addrs():
-    users = User.query.all()
-    num_users = len(users)
-    for num_user, user in enumerate(users):
-        old = user.email
-        if old:
-            new = change_email_addr(addr=old)
-            print "User [%s] of [%s]: changing [%s] to [%s]" % (num_user + 1, num_users, old, new)
-            user.email = new
-        else:
-            print "  no email address for user [%s] [%s] so skipping." % (user.user_name, user.full_name)
-    print "Committing..."
+    for user in User.query.all():
+        user.email = None
     db.session.commit()
-    print "Done."
 
 
 def warning():
@@ -51,9 +39,12 @@ def warning():
         print "Continuing..."
     else:
         sys.exit(1)
+
+
 def main():
     args = parser.parse_args()
     change_user_email_addrs()
+
 
 if __name__ == '__main__':
     warning()
