3527abee015cc94e8b671502e43f38708825afa3
Matt Youell <matt@newmoniclabs.com>
2014-05-12 22:52:19 -0700

    Fixing smoke test to work with in-memory database and a real user.

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,8 +1,26 @@
 import unittest
 from datetime import datetime
 from buddyup import app
+from buddyup.database import User, Visit, db
 
 class SmokeTests(unittest.TestCase):
+
+    def setUp(self):
+        db.create_all()
+
+        existing_user = User(user_name="skippy")
+        existing_user.initialized = True
+        db.session.add(existing_user)
+        db.session.commit()
+
+    def tearDown(self):
+        import os
+        if 'DATABASE_URL' in os.environ and os.environ['DATABASE_URL'] != 'sqlite:///:memory:':
+            os.remove(os.environ['DATABASE_URL'])
+        else:
+            # Delete users from the memory database, if used.
+            User.query.delete()
+
 
     def test_homepage(self):
         # This doesn't test a running server, only the local code base in situ.
