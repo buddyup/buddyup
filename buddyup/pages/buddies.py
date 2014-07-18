4914c4993eb001b200c60fe2ce338b1f69e985e1
Matt Youell <matt@newmoniclabs.com>
2014-07-17 21:27:22 -0700

    Perhaps it would be more useful to show the person's courses instead of the user's courses.

--- buddyup/pages/buddies.py
+++ buddyup/pages/buddies.py
@@ -130,4 +130,4 @@ def green_buddies():
             if user not in buddies:
                 general.add(user)
     classmates = shuffled(general) + shuffled(buddies)
-    return render_template('green/buddies/index.html', classmates=classmates, courses=courses)
+    return render_template('green/buddies/index.html', classmates=classmates)
