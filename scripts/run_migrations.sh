3f549b7d161a42f9dbdc19e70768c1c435503c20
Steven Skoczen <steven@quantumimagery.com>
2014-12-11 15:00:18 +0700

    Fixes from CR

---
+++ scripts/run_migrations.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+heroku run alembic upgrade head --app hudson-buddyup
+heroku run alembic upgrade head --app buddyup
+heroku run alembic upgrade head --app oregonstate-buddyup
+heroku run alembic upgrade head --app ecampus-oregonstate-buddyup
+heroku run alembic upgrade head --app buddyup
+heroku run alembic upgrade head --app buddyup-skylinecollege
+heroku run alembic upgrade head --app buddyup-canadacollege
+heroku run alembic upgrade head --app buddyup-collegeofsanmateo
