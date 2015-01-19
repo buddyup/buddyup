fc947a2abc3b0e8c51d67e3efef2a85d960a3f82
Steven Skoczen <steven@quantumimagery.com>
2015-01-19 18:19:48 +0700

    Adds skip course option

--- scripts/import-courses.py
+++ scripts/import-courses.py
@@ -12,6 +12,10 @@ parser = argparse.ArgumentParser(description="Load a school's course, language, 
 parser.add_argument('school_name', metavar='N', type=str, nargs='+',
                    help='the school name (single word, data prefix)')
 
+parser.add_argument('--skip-courses', dest='skip_courses', action='store_const',
+                   const=True, default=False,
+                   help='Skip course import')
+
 
 MIN_ENROLLMENT = 40
 LANGUAGES = [
@@ -132,28 +136,29 @@ MAJORS = [
 
 def main():
     args = parser.parse_args()
-    with open(os.path.join(os.getcwd(), 'data/%s.csv' % (args.school_name[0])), 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        courses = {}
-        for row in reader:
-            name = u"%s" %row[1]
-            num = int(row[0])
-            if name in courses:
-                courses[name]["num_students"] += num
-            elif name != "":
-                courses[name] = {"num_students": num}
+    if not args.skip_courses:
+        with open(os.path.join(os.getcwd(), 'data/%s.csv' % (args.school_name[0])), 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            courses = {}
+            for row in reader:
+                name = u"%s" %row[1]
+                num = int(row[0])
+                if name in courses:
+                    courses[name]["num_students"] += num
+                elif name != "":
+                    courses[name] = {"num_students": num}
 
-    print "Adding courses.."
-    for course_name, data in courses.iteritems():
-        if data["num_students"] > MIN_ENROLLMENT:
-            print "  %s (%s students):" % (course_name, data["num_students"]),
-            matches = Course.query.filter(Course.name==course_name)
-            if matches.count() > 0:
-                print "exists."
-            else:
-                c = Course(name=course_name)
-                db.session.add(c)
-                print "added."
+        print "Adding courses.."
+        for course_name, data in courses.iteritems():
+            if data["num_students"] > MIN_ENROLLMENT:
+                print "  %s (%s students):" % (course_name, data["num_students"]),
+                matches = Course.query.filter(Course.name==course_name)
+                if matches.count() > 0:
+                    print "exists."
+                else:
+                    c = Course(name=course_name)
+                    db.session.add(c)
+                    print "added."
 
     print "Adding languages.."
     for language in LANGUAGES:
